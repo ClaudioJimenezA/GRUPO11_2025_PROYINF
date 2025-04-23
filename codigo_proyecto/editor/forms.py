10ba8ed4a24675b890ac82c59771babb38aff2de
ClaudioJimenezA <claudio.jimeneza@sansano.usm.cl>
2025-04-23 00:11:12 -0400

    editor- subida de imagenes

--- codigo_proyecto/editor/forms.py
+++ codigo_proyecto/editor/forms.py
@@ -1,49 +1,13 @@
 from django import forms
 from .models import Boletin
-from tinymce.widgets import TinyMCE
-from django.db import models
-from tinymce.models import HTMLField
+
 
 
 class BoletinForm(forms.ModelForm):
-    ESTADOS = (
-        ('borrador', 'Borrador'),
-        ('publicado', 'Publicado'),
-    )
-
-    titulo = models.CharField(max_length=255)
-    contenido = forms.CharField(
-        widget=TinyMCE(
-            mce_attrs={
-                'plugins': (
-                    "advlist autolink lists link image charmap print preview anchor "
-                    "searchreplace visualblocks code fullscreen "
-                    "insertdatetime media table paste code help wordcount autosave"
-                ),
-                'toolbar': (
-                    "undo redo | formatselect | bold italic backcolor | "
-                    "alignleft aligncenter alignright alignjustify | "
-                    "bullist numlist outdent indent | removeformat | help | "
-                    "fullscreen | link image media | code"
-                ),'style_formats': [
-                    {'title': 'Título principal', 'block': 'h1'},
-                    {'title': 'Subtítulo', 'block': 'h2'},
-                    {'title': 'Párrafo normal', 'block': 'p'},
-                    {'title': 'Cita', 'block': 'blockquote'},
-                ],
-
-                'autosave_interval': '10s',
-                'autosave_retention': '2m',
-                'menubar': True,
-                'height': 600,
-            }
-        )
-    )
-    estado = models.CharField(max_length=10, choices=ESTADOS, default='borrador')
-    fecha_creacion = models.DateTimeField(auto_now_add=True)
-    fecha_actualizacion = models.DateTimeField(auto_now=True)
-    
-
     class Meta:
         model = Boletin
         fields = ['titulo', 'contenido']
+        widgets = {
+            'contenido': forms.Textarea(attrs={'id': 'id_contenido'}),
+        }
+
