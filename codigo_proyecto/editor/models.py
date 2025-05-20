8d5b30a1b8c17f2df7dc7e22a49c1a367fa74da4
ClaudioJimenezA <claudio.jimeneza@sansano.usm.cl>
2025-05-20 10:09:19 -0400

    tinymce min js y plantillas de articulos

--- codigo_proyecto/editor/models.py
+++ codigo_proyecto/editor/models.py
@@ -13,3 +13,9 @@ class Boletin(models.Model):
 
     def __str__(self):
         return self.titulo
+    
+class Plantilla(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    contenido_html = models.TextField()
+
