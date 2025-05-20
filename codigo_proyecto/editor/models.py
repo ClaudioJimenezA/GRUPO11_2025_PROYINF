f7543e7d92e8449d922f42b7822b6254da88dc18
benjamin-Olguin <benjamin.olguin@usm.cl>
2025-05-20 01:56:28 -0400

    inicio de sesion, control via clase de usuario

--- codigo_proyecto/editor/models.py
+++ codigo_proyecto/editor/models.py
@@ -9,3 +9,16 @@ class Boletin(models.Model):
 
     def __str__(self):
         return self.titulo
+
+
+class SupabaseUser(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    created_at = models.DateTimeField()
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    user_class = models.CharField(max_length=255, db_column='class')  # "class" es palabra reservada
+    email = models.CharField(max_length=255, null=True, db_column='email')
+
+    class Meta:
+        db_table = 'users'
+        managed = False  # Importante: no dejar que Django cree/modifique esta tabla
