493f2731b8bfc5b2a57339759f8f83ed9c4e3162
ClaudioJimenezA <130109134+ClaudioJimenezA@users.noreply.github.com>
2025-06-23 17:48:24 -0400

    borrar codigo comentado de views.py

--- codigo_proyecto/buscador_fuentes/views.py
+++ codigo_proyecto/buscador_fuentes/views.py
@@ -70,37 +70,3 @@ def buscar_fuentes(request):
         'etiquetas': etiquetas,
     })
 
-"""
-def agregar_fuente(request):
-    if request.method == 'POST':
-        nombre = request.POST['nombre']
-        link = request.POST['link']
-        etiquetas = request.POST['etiquetas'].split(',')
-        GestorFuentes.agregar_fuente(nombre, link, etiquetas)
-        return redirect('vista_fuentes')
-
-    return render(request, 'buscador_fuentes/agregar_fuente.html')
-
-def buscar_fuentes(request):
-    etiqueta = request.GET.get('etiqueta', '')
-    fuentes = GestorFuentes.buscar_fuentes_por_etiqueta(etiqueta)
-    return render(request, 'buscador_fuentes/buscar_fuentes.html', {'fuentes': fuentes})
-
-def modificar_fuente(request, nombre):
-    if request.method == 'POST':
-        nuevo_nombre = request.POST.get('nuevo_nombre')
-        nuevo_link = request.POST.get('nuevo_link')
-        nuevas_etiquetas = request.POST.get('nuevas_etiquetas').split(',')
-        GestorFuentes.modificar_fuente(nombre, nuevo_nombre, nuevo_link, nuevas_etiquetas)
-        return redirect('vista_fuentes')
-
-    return render(request, 'buscador_fuentes/modificar_fuente.html', {'nombre': nombre})
-
-def eliminar_fuente(request, nombre):
-    GestorFuentes.eliminar_fuente(nombre)
-    return redirect('vista_fuentes')
-
-def vista_fuentes(request):
-    fuentes = GestorFuentes.mostrar_fuentes()
-    return render(request, 'buscador_fuentes/vista_fuentes.html', {'fuentes': fuentes})
-"""
